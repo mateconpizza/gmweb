3456ed7d684030bac3f3d28ae2d17fb6a12de0ad
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2025-07-27 12:20:05 -0300

    feat: add `API` implementation

---
+++ api/api.go
@@ -0,0 +1,94 @@
+// Package api contains the API server implementation.
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	ErrWMNoVersionAvailable = errors.New("wayback machine: no version available")
+	ErrServerAlreadyRunning = errors.New("server is already running")
+)
+
+type Middleware func(http.Handler) http.Handler
+
+type OptFn func(*apiOpt)
+
+type apiOpt struct {
+	addr        string
+	router      *http.ServeMux
+	middlewares []Middleware
+	certFile    string
+	keyFile     string
+}
+
+type API struct {
+	*http.Server
+	isActive int32
+	*apiOpt
+}
+
+func WithMux(r *http.ServeMux) OptFn {
+	return func(o *apiOpt) {
+		o.router = r
+	}
+}
+
+func WithAddr(addr string) OptFn {
+	return func(o *apiOpt) {
+		o.addr = addr
+	}
+}
+
+func WithMiddleware(mw ...Middleware) OptFn {
+	return func(o *apiOpt) {
+		o.middlewares = append(o.middlewares, mw...)
+	}
+}
+
+func WithTLS(certFile, keyFile string) OptFn {
+	return func(o *apiOpt) {
+		o.certFile = certFile
+		o.keyFile = keyFile
+	}
+}
+
+func New(opts ...OptFn) *API {
+	o := &apiOpt{
+		router: http.NewServeMux(),
+		addr:   ":8080",
+	}
+
+	for _, optFn := range opts {
+		optFn(o)
+	}
+
+	var handler http.Handler = o.router
+	for i := len(o.middlewares) - 1; i >= 0; i-- {
+		handler = o.middlewares[i](handler)
+	}
+
+	return &API{
+		Server: &http.Server{
+			Addr:         o.addr,
+			Handler:      handler,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 30 * time.Second,
+			IdleTimeout:  60 * time.Second,
+		},
+		apiOpt: o,
+	}
+}
+
+func (a *API) Start() error {
+	if !atomic.CompareAndSwapInt32(&a.isActive, 0, 1) {
+		return ErrServerAlreadyRunning
+	}
+
+	fmt.Printf("Server running on http://localhost%s\n", a.addr)
+	return a.ListenAndServeTLS(a.certFile, a.keyFile)
+}
---
+++ api/response.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
+
+type ResponseData struct {
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
+}
+
+type ResponseError struct {
+	Error      string `json:"error"`
+	StatusCode int    `json:"status_code"`
+}
+
+type FetchDataResponse struct {
+	Title      string   `json:"title"`
+	Desc       string   `json:"desc"`
+	Tags       []string `json:"tags"`
+	FaviconURL string   `json:"favicon"`
+}
+
+type QRCodeResponse struct {
+	URL     string `json:"url"`
+	Base64  string `json:"base64"`
+	MIME    string `json:"mime"`
+	Message string `json:"message,omitempty"`
+}
+
+type QRCodeRequest struct {
+	URL  string `json:"url"`
+	Size int    `json:"size"`
+}
+
+type RepoStatsResponse struct {
+	Name      string `json:"name"`
+	Bookmarks int    `json:"bookmarks"`
+	Tags      int    `json:"tags"`
+	Favorites int    `json:"favorites"`
+}
+
+func encodeErrJSON(w http.ResponseWriter, statusCode int, err string) {
+	w.WriteHeader(statusCode)
+	if err := json.NewEncoder(w).Encode(&ResponseError{Error: err, StatusCode: statusCode}); err != nil {
+		slog.Error("encoding error", "error", err)
+	}
+}
