14095f917faaadbd568f0685abb4d8482baed8d2
mateconpizza <81921095+mateconpizza@users.noreply.github.com>
2025-08-04 12:51:33 -0300

    feat: add `forms` package
    - add `github.com/go-playground/form/v4` pkg

---
+++ internal/forms/forms.go
@@ -0,0 +1,59 @@
+package forms
+
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	form "github.com/go-playground/form/v4"
+)
+
+type BookmarkCreate struct {
+	URL         string
+	Tags        []string
+	Title       string
+	Description string
+	FaviconURL  string `db:"favicon_url"`
+	CreatedAt   time.Time
+	Validator   `form:"-"`
+}
+
+type UserSignUp struct {
+	Name      string `form:"name"`
+	Email     string `form:"email"`
+	Password  string `form:"password"`
+	Validator `form:"-"`
+}
+
+type UserLogin struct {
+	Name      string `form:"name"`
+	Password  string `form:"password"`
+	Validator `form:"-"`
+}
+
+func DecodePostForm(r *http.Request, dst any) error {
+	err := r.ParseForm()
+	if err != nil {
+		return err
+	}
+
+	// Call Decode() on our decoder instance, passing the target destination as
+	// the first parameter.
+	decoder := form.NewDecoder()
+	err = decoder.Decode(dst, r.PostForm)
+	if err != nil {
+		// If we try to use an invalid target destination, the Decode() method
+		// will return an error with the type *form.InvalidDecoderError.We use
+		// errors.As() to check for this and raise a panic rather than returning
+		// the error.
+		var invalidDecoderError *form.InvalidDecoderError
+		if errors.As(err, &invalidDecoderError) {
+			panic(err)
+		}
+
+		// For all other errors, we return them as normal.
+		return err
+	}
+
+	return nil
+}
---
+++ internal/forms/validator.go
@@ -0,0 +1,58 @@
+package forms
+
+import (
+	"slices"
+	"strings"
+	"unicode/utf8"
+)
+
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
+// Valid returns true if the FieldErrors map doesn't contain any entries.
+func (v *Validator) Valid() bool {
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+func (v *Validator) AddFieldError(key, message string) {
+	// Note: We need to initialize the map first, if it isn't already
+	// initialized.
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[key]; !exists {
+		v.FieldErrors[key] = message
+	}
+}
+
+func (v *Validator) CheckField(ok bool, key, message string) {
+	if !ok {
+		v.AddFieldError(key, message)
+	}
+}
+
+// NotBlank returns true if a value is not an empty string.
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+// MinChars returns true if a value contains at least n characters.
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+// MaxChars returns true if a value contains no more than n characters.
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
+}
